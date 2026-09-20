602802ec929a21389e4f21767ca5ea58310b14ce
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-20 20:45:47 +0530

    Added hw.cpp

---
+++ 01_Basics/hw.cpp
@@ -0,0 +1,17 @@
+// 1 n 2 n-1 3 n-2
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter n:";
+    cin >> n;
+
+    int a = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << a << " " << "n" - i + 1 << " ";
+        a = a + 1;
+    }
+}

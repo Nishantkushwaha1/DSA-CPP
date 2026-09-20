5365baedbb1407bc0e81d917c64b15ecf48eda5b
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-20 20:44:36 +0530

    Added Good_Morning.cpp

---
+++ 01_Basics/Good_Morning.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    // for(int i=1; i<=n; i++)
+    // {
+    //     cout<<i<<" Nishant Kushwaha"<<endl;
+    // }
+    int i=1;
+    while(i<=n)
+    {
+        cout<<"Nishant Kushwaha"<<endl;
+        i++;
+    }
+
+
+    
+}
+
+

4b036a5cbf62bb2d92afa1d7d59023a1241eebc0
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-21 23:15:30 +0530

    Added Range_of_int.cpp

---
+++ 01_Basics/Range_of_int.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+int main()
+{
+    int x = INT_MAX;
+    cout << x << endl;
+
+    int y = INT_MIN;
+    cout << y << endl;
+
+    long long a = LLONG_MAX, b = LLONG_MIN;
+    cout << a << endl;
+    cout << b << endl;
+}

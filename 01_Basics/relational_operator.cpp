a2805967173c58b69c27b4cf26adb323ecad2949
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-21 23:16:01 +0530

    Added relational_operator

---
+++ 01_Basics/relational_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool x = false;     //false is 0
+    bool y = true;      //true is 1
+    cout << x << endl;
+    cout << y << endl;
+
+}

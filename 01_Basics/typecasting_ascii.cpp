aea72bda5252d87ec897a1f467037994cb9b8fd0
Nishant <nishantkushwahadeoria@gmail.com>
2026-09-26 00:38:32 +0530

    added typecasting.cpp

---
+++ 01_Basics/typecasting_ascii.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    
+    // char x='A';
+    // int ascii= int(x);  //Explicit Typecasting
+    // cout<<ascii;  
+
+    // char x='a';
+    // int ascii= x;   //Implicit Typecasting
+    // cout<<ascii;
+
+
+    int x=74;
+    char ascii=(char)x;
+    cout<<ascii;
+   
+
+}
